14ea0d561b6362a48f1d45ee72c15c6117b3fe54
Sila Saritepeci <saritepecisila@gmail.com>
2023-02-05 14:15:21 +0100

    05.02.2023

--- src/test/java/team_16/pages/SignIn.java
+++ src/test/java/team_16/pages/SignIn.java
@@ -1,5 +1,7 @@
 package team_16.pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import team_16.utilities.Driver;
 
@@ -8,6 +10,8 @@ public class SignIn {
     public  SignIn () {
         PageFactory.initElements(Driver.getDriver(),this);
     }
+    @FindBy(xpath ="(//*[@class='w-icon-account'])[1]")
+    public WebElement sigInButton;
 
 
 }
