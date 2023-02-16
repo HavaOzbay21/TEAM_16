307d5f0ec3e873062c690133eb404d61af0a6ba8
Hamit <hamitsvsy@gmail.com>
2023-02-16 22:33:51 +0100

    v1

--- src/test/java/team_16/tests/us08/US_09.java
+++ src/test/java/team_16/tests/us08/US_09.java
@@ -1,7 +1,10 @@
 package team_16.tests.us08;
 
+import org.apache.poi.ss.formula.functions.T;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import team_16.pages.*;
@@ -9,8 +12,11 @@ import team_16.utilities.ConfigReader;
 import team_16.utilities.Driver;
 import team_16.utilities.ReusableMethods;
 
+import java.security.Key;
+
 public class US_09 {
 
+    VendorRegistration vendorRegistration = new VendorRegistration();
 
     SignIn signIn = new SignIn();
     SignUp signUp = new SignUp();
@@ -19,9 +25,10 @@ public class US_09 {
     ShoppingCard shoppingCard = new ShoppingCard();
     BillingDetails billingDetails = new BillingDetails();
     MyAccount myAccount = new MyAccount();
+    Actions actions = new Actions(Driver.getDriver());
 
-    @Test (priority = 1)
-    public  void test01() {
+    @Test(priority = 1)
+    public void test01() {
         //*****US09*****
         //*****TC01*****
         //1-)Kullanici https://spendinggood.com/ url'sine gider
@@ -37,8 +44,9 @@ public class US_09 {
         WebElement vendorRegistrationYazısı = Driver.getDriver().findElement(By.xpath("//h2[.='Vendor Registration']"));
         Assert.assertTrue(vendorRegistrationYazısı.isDisplayed());
     }
-    @Test (priority = 2)
-    public  void test02() {
+
+    @Test(priority = 2)
+    public void test02() throws InterruptedException {
         //*****US09*****
         //*****TC02*****
         //1-)Vendor Registration sayfasına ulaşır
@@ -47,31 +55,75 @@ public class US_09 {
         Assert.assertTrue(vendorRegistrationYazısı.isDisplayed());
         //2-)Kullanıcı Registration sayfasında yer alan E-mail kutusunu görüntülemeli
 
-        WebElement RegistrationEmailKutusu =Driver.getDriver().findElement(By.xpath("//input[@name='user_email']"));
-        Assert.assertTrue(RegistrationEmailKutusu.isDisplayed());
+        WebElement registrationEmailKutusu = Driver.getDriver().findElement(By.xpath("//input[@name='user_email']"));
+        Assert.assertTrue(registrationEmailKutusu.isDisplayed());
 
         //3-)Kullanıcı Mail adresini doğru şekilde girer
-        ReusableMethods.clickByJS(RegistrationEmailKutusu);
-        RegistrationEmailKutusu.sendKeys("hamittechpro@gmail.com");
+        Driver.getDriver().get("https://www.fakemail.net/");
+        Thread.sleep(3000);
+        String emailcopyelde=Driver.getDriver().findElement(By.xpath("//span[@id='email']")).getText();
+        System.out.println("giriş yaptığımız email : "+emailcopyelde);
+
+        WebElement copyfakemail = Driver.getDriver().findElement(By.xpath("//div[@style='text-align:center;padding:9px;font-size:20px;color:white;background:#ff99cc;']"));
+        copyfakemail.click();
 
 
+        Thread.sleep(3000);
+        Driver.getDriver().navigate().back();//register sayfasına geri geldik
+       // Driver.getDriver().get("https://spendinggood.com/vendor-register/");
+        Thread.sleep(3000);
+        registrationEmailKutusu.click();
+        Thread.sleep(3000);
+        registrationEmailKutusu.sendKeys(Keys.CONTROL+"v");//yapıştır
+
+        Thread.sleep(3000);
+        Driver.getDriver().navigate().forward();
+        Thread.sleep(10000);
+        Driver.getDriver().findElement(By.xpath("//tr[@class='hidden-xs hidden-sm klikaciRadek newMail']")).click();
+        Thread.sleep(3000);
+        WebElement fakecode = Driver.getDriver().findElement(By.id("predmet"));
+        Thread.sleep(3000);
+        System.out.println(fakecode.getText());
+        String fakecodetext = fakecode.getText();
+        String code =fakecodetext.split(" ")[6];
+        System.out.println("giriş yaptığımız code : "+code);
+        Driver.getDriver().navigate().back();
+        Driver.getDriver().findElement(By.xpath("//input[@name='wcfm_email_verified_input']")).sendKeys(code);
 
 
+        vendorRegistration.password.sendKeys("asdf1234");
+        vendorRegistration.Confirm_Password.sendKeys("asdf1234");
+        Thread.sleep(3000);
+        ReusableMethods.clickByJS(vendorRegistration.Register_Button);
+        Thread.sleep(5000);
+        Driver.getDriver().navigate().back();
+        assert vendorRegistration.Completed_Yazısı.isDisplayed();
+        Thread.sleep(5000);
+        Driver.getDriver().navigate().refresh();
 
+
+        Thread.sleep(5000);
+        actions.sendKeys(Keys.PAGE_UP).sendKeys(Keys.PAGE_UP).perform();
+        WebElement signout= Driver.getDriver().findElement(By.xpath("(//i[@class='w-icon-account'])[1]"));
+        ReusableMethods.clickByJS(signout);
+        Thread.sleep(3000);
+        WebElement logout= Driver.getDriver().findElement(By.xpath("//span[@class='icon-box-icon icon-logout']"));
+        ReusableMethods.clickByJS(logout);
+        Thread.sleep(3000);
+        Driver.getDriver().get("https://spendinggood.com/vendor-register/");
+        Thread.sleep(5000);
+        WebElement asd = Driver.getDriver().findElement(By.xpath("//input[@name='user_email']"));
+        Thread.sleep(3000);
+        asd.sendKeys(Keys.CONTROL+"v");//yapıştır
+        Driver.getDriver().findElement(By.xpath("//input[@name='wcfm_email_verified_input']")).sendKeys(code);
+        vendorRegistration.password.sendKeys("asdf1234");
+        vendorRegistration.Confirm_Password.sendKeys("asdf1234");
+        Thread.sleep(3000);
+        ReusableMethods.clickByJS(vendorRegistration.Register_Button);
+        WebElement existsyazısı=Driver.getDriver().findElement(By.xpath("//div[@class='wcfm-message wcfm-error']"));
+        assert existsyazısı.isDisplayed();
 
 
     }
-
-
-
-
-
-
-
-
-
-
-
-
 
 }
