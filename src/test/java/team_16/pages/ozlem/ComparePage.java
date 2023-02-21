cbebb155d13d4e1add5ec136a988a4e0f7670c67
Ozlem Asimoglu <Ozlemasimoglu@gmail.com>
2023-02-21 22:35:21 +0100

    v

--- src/test/java/team_16/pages/ozlem/ComparePage.java
+++ src/test/java/team_16/pages/ozlem/ComparePage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import team_16.utilities.Driver;
 
-public class ComparePage{
+public class git ComparePage{
 
     public ComparePage()
     {
