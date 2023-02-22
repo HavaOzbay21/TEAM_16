16fed02ecb9da773c95ab77f35b1186a0db036d9
Fatih KASIKCI <fatih.techproe@gmail.com>
2023-02-22 22:14:52 +0000

    22/02/2023

--- src/test/java/team_16/pages/BillingDetails.java
+++ src/test/java/team_16/pages/BillingDetails.java
@@ -36,7 +36,7 @@ public class BillingDetails {
     public WebElement placeOrder;
 
     @FindBy(xpath = "//*[.='Enter your code']")
-    public WebElement enterYourCode;
+    public WebElement enterYourCode2;
 
     @FindBy(xpath = "//*[@id=\"coupon_code\"]")
     public WebElement codeBoslugu;
